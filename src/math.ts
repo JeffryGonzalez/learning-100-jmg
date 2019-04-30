9426677046b9ef449040ac50580a93c638837352
Jeff Gonzalez <jeff@Hypertheory>
2019-04-30 11:21:33 -0400

    initial

---
+++ src/math.ts
@@ -0,0 +1,18 @@
+
+
+// public int Add(int a, int b)
+export function add(a: number, b: number) {
+    return a + b;
+}
+
+export function subtract(a: number, b: number) {
+    return a - b;
+}
+
+export function multiply(a: number, b: number) {
+    return a * b;
+}
+
+export function divide(a: number, b: number) {
+    return a / b;
+}
